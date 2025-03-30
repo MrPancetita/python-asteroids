679c44dd27824dab14f0027a5b78a4d59a2b964e
MrPancetita <contreras.engineer@outlook.com>
2025-03-30 22:57:01 +0200

    ch2-l1 ready

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import pygame
+from constants import *  # Importing all constants from the constants module
 
 # This line ensures the main() function is only called when this file is run directly; it won't run if it's imported as a module
 
 def main():
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
