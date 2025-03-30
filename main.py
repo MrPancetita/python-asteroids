4cc6256d38f52ee4df1454f503c42baead87513f
MrPancetita <contreras.engineer@outlook.com>
2025-03-30 22:53:38 +0200

    ch1-l3 ready-fixed

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 
 # This line ensures the main() function is only called when this file is run directly; it won't run if it's imported as a module
 
+def main():
+    print("Starting Asteroids!")
+
 if __name__ == "__main__":
     main()
-
