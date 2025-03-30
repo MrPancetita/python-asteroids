a94e79b5b0a6d5ca80b1b9860c724f0d21d1caed
MrPancetita <contreras.engineer@outlook.com>
2025-03-30 22:52:16 +0200

    ch1-l3 ready

---
+++ main.py
@@ -0,0 +1,7 @@
+import pygame
+
+# This line ensures the main() function is only called when this file is run directly; it won't run if it's imported as a module
+
+if __name__ == "__main__":
+    main()
+
